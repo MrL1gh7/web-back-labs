1cd77dafde84885e4adf500030d9796f6bbee224
Окачутин Вячеслав <ckfdf.slava00@gmail.com>
2025-09-24 23:20:45 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,10 +2,25 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/web")
 def start():
-    return "<!doctype html>" \
-        "<html>" \
-        "   <body>" \
-        "       <h1>web-сервер на flask</h1>" \
-        "   </body>" \
-        "</html>"
+    return """<!doctype html>
+        <html> 
+           <body> 
+               <h1>web-сервер на flask</h1> 
+           </body> 
+        </html>"""
+@app.route("/author")
+def author():
+    name = "Окачутин Вячеслав Владимирович"
+    group = "ФБИ-33"
+    faculty = "ФБ"
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href='/web'>web</a>
+            </body>
+        </html>"""
