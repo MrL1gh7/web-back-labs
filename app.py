16db7901371ddbb765c5919da03ba8ca47fabb28
Окачутин Вячеслав <ckfdf.slava00@gmail.com>
2025-10-24 13:24:29 +0700

    Заготовка к третьей лабораторной работе

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, url_for, request, redirect, abort, render_template
 import datetime
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 
 @app.route("/")
 @app.route("/index")
@@ -20,6 +22,7 @@ def index():
                 <ul>
                     <li><a href='/lab1'>Первая лабораторная</a></li>
                     <li><a href='/lab2'>Вторая лабораторная</a></li>
+                    <li><a href='/lab3'>Третья лабораторная</a></li>
                 </ul>
                 <footer>
                     Окачутин Вячеслав Владимирович, ФБИ-33, 3 курс, 2025
