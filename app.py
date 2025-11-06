207462c26c340c371d114a524c4238aa3bf6c292
Окачутин Вячеслав <ckfdf.slava00@gmail.com>
2025-11-06 21:28:10 +0700

    Дополнительное задание

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import datetime
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 app = Flask(__name__)
+app.secret_key = 'Секретно-секретный ключ'
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 @app.route("/")
 @app.route("/index")
@@ -23,6 +26,7 @@ def index():
                     <li><a href='/lab1'>Первая лабораторная</a></li>
                     <li><a href='/lab2'>Вторая лабораторная</a></li>
                     <li><a href='/lab3'>Третья лабораторная</a></li>
+                    <li><a href='/lab4'>Четвертая лабораторная</a></li>
                 </ul>
                 <footer>
                     Окачутин Вячеслав Владимирович, ФБИ-33, 3 курс, 2025
