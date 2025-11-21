1d8244e7ccb46dde48a050c5475bc54f103e7f78
Окачутин Вячеслав <ckfdf.slava00@gmail.com>
2025-11-21 15:16:52 +0700

    Лабораторная работа 5

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask, url_for, request, redirect, abort, render_template
-import datetime
+from flask import Flask, url_for, request, redirect, abort, render_template, session
+import os
+from datetime import datetime
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
+from lab5 import lab5
 app = Flask(__name__)
-app.secret_key = 'Секретно-секретный ключ'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
+app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
+app.register_blueprint(lab5)
 
 @app.route("/")
 @app.route("/index")
@@ -27,6 +31,7 @@ def index():
                     <li><a href='/lab2'>Вторая лабораторная</a></li>
                     <li><a href='/lab3'>Третья лабораторная</a></li>
                     <li><a href='/lab4'>Четвертая лабораторная</a></li>
+                    <li><a href='/lab5'>Пятая лабораторная</a></li>
                 </ul>
                 <footer>
                     Окачутин Вячеслав Владимирович, ФБИ-33, 3 курс, 2025
