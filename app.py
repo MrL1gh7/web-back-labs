a81d6a5235f3e45acde46565d100efe4c44f845f
Окачутин Вячеслав <ckfdf.slava00@gmail.com>
2025-12-30 00:38:47 +0700

    Восьмая лабораторная работа

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, url_for, request, redirect, abort, render_template, session
 import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+from db.models import users
+from flask_login import LoginManager
+
 from datetime import datetime
 from lab1 import lab1
 from lab2 import lab2
@@ -8,9 +14,37 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 app = Flask(__name__)
+
+login_manager = LoginManager()
+login_manager.login_view = 'lab8.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return users.query.get(int(user_id))
+
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'andrey_gorshkov_orm'
+    db_user = 'andrey_gorshkov_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "ivan_ivanov_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+    
+db.init_app(app)
+
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
@@ -18,6 +52,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 
 @app.route("/")
 @app.route("/index")
@@ -38,6 +73,7 @@ def index():
                     <li><a href='/lab5'>Пятая лабораторная</a></li>
                     <li><a href='/lab6'>Шестая лабораторная</a></li>
                     <li><a href='/lab7'>Седьмая лабораторная</a></li>
+                    <li><a href='/lab8'>Восьмая лабораторная</a></li>
                 </ul>
                 <footer>
                     Окачутин Вячеслав Владимирович, ФБИ-33, 3 курс, 2025
